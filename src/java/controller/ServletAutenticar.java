7760db4fdbd2be7aa2c35b46309e34bdc3ee2e45
rapid <rapid@DESKTOP-Q15TMRP>
2017-03-25 10:41:40 -0600

    Creacion del modelo

--- src/java/controller/ServletAutenticar.java
+++ src/java/controller/ServletAutenticar.java
@@ -21,9 +21,6 @@ public class ServletAutenticar extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("Application/vnd.ms-excel");
-     PrintWriter salida=     response.getWriter();
-     salida.println("Yo soy Juan Carlos ");
-        
+  
     }
 }
