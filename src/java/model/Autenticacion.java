0e32d6c686d848b47a6d6a8c61ba0365431bc8ba
rapid <rapid@DESKTOP-Q15TMRP>
2017-04-05 21:41:07 -0500

    Merge origin/master
    Conflicts:
    	README.md

--- src/java/model/Autenticacion.java
+++ src/java/model/Autenticacion.java
@@ -4,10 +4,22 @@
  * and open the template in the editor.
  */
 package model;
+
 import java.sql.*;
 
 public class Autenticacion {
-    
-    
-    
+
+    public static  boolean autenticar(String login, String password) {
+        boolean autenticado=true;
+        try {
+
+            ModeloConexion.conectarse(login, password);
+            autenticado=true;
+            System.out.println("malo");
+        }catch (Exception e) {
+            autenticado = false;
+        }
+        return autenticado;
+    }
+
 }
--- src/java/model/ModeloConexion.java
+++ src/java/model/ModeloConexion.java
@@ -12,7 +12,7 @@ public class ModeloConexion {
     public static Connection conectarse(String login,
             String password)throws Exception{
         
-        String url="jdbc:orcle:thin@localhost:1521:xe";
+        String url="jdbc:oracle:thin:@localhost:1521:xe";
             Class.forName("oracle.jdbc.OracleDriver");
     Connection con= DriverManager.
             getConnection(url, login, password);
