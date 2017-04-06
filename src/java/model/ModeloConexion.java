ca21ce2cdaee3821ea36ee89740a22e1c9794a39
rapid <rapid@DESKTOP-Q15TMRP>
2017-04-05 20:32:12 -0500

    asasas

--- src/java/model/ModeloConexion.java
+++ src/java/model/ModeloConexion.java
@@ -9,4 +9,14 @@ import java.sql.*;
 
 public class ModeloConexion {
     
+    public static Connection conectarse(String login,
+            String password)throws Exception{
+        
+        String url="jdbc:orcle:thin@localhost:1521:xe";
+            Class.forName("oracle.jdbc.OracleDriver");
+    Connection con= DriverManager.
+            getConnection(url, login, password);
+     return con;
+    }
+    
 }
